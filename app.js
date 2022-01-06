e9ea520babbb27ae03e22e72cd6004fa2918efc1
Andrés  Díaz Vargas <andres.diaz.vargas@gorillalogic.com>
2022-01-05 19:06:24 -0500

    using finished styles

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 require('dotenv').config()
+
+const logger = require('morgan')
 const express = require('express')
+const errorHandler = require('errorhandler')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
 const app = express()
 const path = require('path')
 const port = 3000
 
-const logger = require('morgan')
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(methodOverride())
+app.use(errorHandler())
+app.use(express.static(path.join(__dirname, 'public')))
+
 const Prismic = require('@prismicio/client')
 const PrismicDOM = require('prismic-dom')
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
-
-const errorHandler = require('errorhandler')
 
 const initApi = req => {
   return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
@@ -19,119 +27,108 @@ const initApi = req => {
   })
 }
 
-const handleLinkResolver = (doc) => {
-  if(doc.type === 'product'){
+const handleLinkResolver = doc => {
+  if (doc.type === 'product') {
     return `/detail/${doc.slug}`
   }
 
-  if(doc.type === 'about'){
-    return `/about`
+  if (doc.type === 'collections') {
+    return '/collections'
   }
 
-  if(doc.type === 'collections'){
-    return `/collections`
+  if (doc.type === 'about') {
+    return '/about'
   }
-
 
   return '/'
 }
 
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(methodOverride())
-app.use(errorHandler());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next)=>{
-  res.locals.ctx = {
-    endpoint: process.env.PRISMIC_ENDPOINT,
-    linkResolver: handleLinkResolver
-  }
-
+app.use((req, res, next) => {
   res.locals.Link = handleLinkResolver
 
-  res.locals.PrismicDOM = PrismicDOM
   res.locals.Numbers = index => {
-    return index===0 ? 'One' : index===1 ? 'Two' : index===2 ? 'Three' : index===3 ? 'Four' : ''
+    return index == 0 ? 'One' : index == 1 ? 'Two' : index == 2 ? 'Three' : index == 3 ? 'Four' : '';
   }
+
+  res.locals.PrismicDOM = PrismicDOM
+
   next()
 })
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-const handleRequest =async api =>{
-  const metadata = await api.getSingle('meta')
-  const preloader = await api.getSingle('preloader')
+const handleRequest = async api => {
+  const meta = await api.getSingle('meta')
   const navigation = await api.getSingle('navigation')
+  const preloader = await api.getSingle('preloader')
+
   return {
-    metadata,
+    meta,
     navigation,
     preloader
   }
 }
 
-app.get('/', async (req, res)=> {
+app.get('/', async (req, res) => {
   const api = await initApi(req)
-  const home = await api.getSingle('about')
   const defaults = await handleRequest(api)
+  const home = await api.getSingle('home')
 
-  const {results: collections} = await api.query(Prismic.Predicates.at('document.type', 'collection'), {
+  const { results: collections } = await api.query(Prismic.Predicates.at('document.type', 'collection'), {
     fetchLinks: 'product.image'
   })
+
+  console.log(defaults.navigation)
 
   res.render('pages/home', {
     ...defaults,
     collections,
-    home,
+    home
   })
 })
 
-app.get('/about', async(req, res)=> {
+app.get('/about', async (req, res) => {
   const api = await initApi(req)
-  const about = await api.getSingle('about')
   const defaults = await handleRequest(api)
-
+  const about = await api.getSingle('about')
 
   res.render('pages/about', {
-    about,
     ...defaults,
+    about
   })
 })
 
-app.get('/detail/:uid', async(req, res)=> {
+app.get('/collections', async (req, res) => {
   const api = await initApi(req)
+  const defaults = await handleRequest(api)
+  const home = await api.getSingle('home')
+
+  const { results: collections } = await api.query(Prismic.Predicates.at('document.type', 'collection'), {
+    fetchLinks: 'product.image'
+  })
+
+  res.render('pages/collections', {
+    ...defaults,
+    collections,
+    home
+  })
+})
+
+app.get('/detail/:uid', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
   const product = await api.getByUID('product', req.params.uid, {
     fetchLinks: 'collection.title'
   })
-  const defaults = await handleRequest(api)
 
-  console.log('el productttt', product.data.highlights)
   res.render('pages/detail', {
-    product,
-    ...defaults
+    ...defaults,
+    product
   })
 })
 
-app.get('/collections', async (req, res)=> {
-  const api = await initApi(req)
-  const {results: collections} = await api.query(Prismic.Predicates.at('document.type', 'collection'), {
-    fetchLinks: 'product.image'
-  })
-  const home = await api.getSingle('home')
-  const defaults = await handleRequest(api)
-
-
-  console.log(collections.data)
-
-  res.render('pages/collections', {
-    collections,
-    home,
-    ...defaults
-  })
-})
-
-app.listen(port, ()=> {
-  console.log(`listening at http://localhost:${port}`)
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
